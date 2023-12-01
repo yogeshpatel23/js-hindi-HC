8bf68d9e02eafff027a7c1a779e42e6f6d68a8af
yogeshpatel23 <40227238+yogeshpatel23@users.noreply.github.com>
2023-12-01 08:38:41 +0000

    learn Datatype

---
+++ 01_basics/02_DataTypes.js
@@ -0,0 +1,24 @@
+"use strict";   // treat all JS code as newer version
+
+// alert( 3 + 3)    // we are using Nodejs, not browser
+
+let name = "Yogesh" // string
+let age = 20  // number
+let isLoggedIn = false // boolean
+
+/**
+ *premative Data Types
+ number
+ bigint
+ string
+ boolean
+ null => standalone value
+ undefined => 
+ symbol => unique
+
+// object
+ */
+
+console.log(typeof null)  // object
+console.log(typeof undefined) // undefined
+
