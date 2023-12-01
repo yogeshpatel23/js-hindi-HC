a1bf326ca67d3708f9cef4fa20029cb5a0b2ced3
yogeshpatel23 <40227238+yogeshpatel23@users.noreply.github.com>
2023-12-01 10:23:21 +0000

    Datatype summery

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,33 @@
+//  Primitive (call by value)
+
+//  7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log([id, anotherId])
+
+// Reference (Non primitive) // (call by ref)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"];
+let myObj = {
+    name: "hitesh",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+console.log(typeof anotherId);
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
