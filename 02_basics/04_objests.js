005fa466353aabb60df90961ab95633e740f6962
Yogesh Patel <yogesh.d.p.patel@gmail.com>
2023-12-01 22:41:49 +0530

    Object and Objest desctuer

--- 02_basics/04_objests.js
+++ 02_basics/04_objests.js
@@ -56,3 +56,24 @@ console.log(tinderUser);
 // console.log(Object.keys(tinderUser));
 // console.log(Object.values(tinderUser));
 // console.log(Object.entries(tinderUser));
+
+const course = {
+  coursename: "js in hindi",
+  price: "999",
+  courseInstructor: "hitesh",
+};
+
+// course.courseInstructor
+
+const { courseInstructor: instructor } = course;
+
+// console.log(courseInstructor);
+console.log(instructor);
+
+// {
+//     "name": "hitesh",
+//     "coursename": "js in hindi",
+//     "price": "free"
+// }
+
+[{}, {}, {}];
