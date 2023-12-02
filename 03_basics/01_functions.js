c6e2d0a1660be50fc1fae1b75e7db7ecb3d8cc58
Yogesh Patel <yogesh.d.p.patel@gmail.com>
2023-12-02 10:36:51 +0530

    More about functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -34,3 +34,35 @@ function loginUserMessage(username = "sam") {
 
 // console.log(loginUserMessage("yogesh"))
 // console.log(loginUserMessage("yogesh"))
+
+function calculateCartPrice(val1, val2, ...num1) {
+  return num1;
+}
+
+// console.log(calculateCartPrice(200, 400, 500, 2000));
+
+const user = {
+  username: "Yogesh",
+  price: 199,
+};
+
+function handleObject(anyobject) {
+  console.log(
+    `user name is ${anyobject.username} and price is${anyobject.price}`
+  );
+}
+
+// handleObject(user);
+handleObject({
+  username: "boocs",
+  price: "sdfese",
+});
+
+const myNewArray = [200, 400, 100, 600];
+
+function returnSecondValue(getArray) {
+  return getArray[1];
+}
+
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 400, 500, 1000]));
