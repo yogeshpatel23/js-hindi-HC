f46c7fee1c8e91a0142e402337dc96c4a1210ae2
Yogesh Patel <yogesh.d.p.patel@gmail.com>
2023-12-02 13:34:02 +0530

    Control flow

---
+++ 04_Control_flow/one.js
@@ -0,0 +1,49 @@
+// if
+const isUserLoggedIn = true;
+const temperature = 41;
+
+// if ( temperature === 40 ){
+//     console.log("less than 50");
+// } else {
+//     console.log("temperature is greater than 50");
+// }
+
+// console.log("Execute");
+
+// < , > , <= , >=, == , != . === . !===
+
+// const score = 200;
+
+// if (score > 100) {
+//   const power = "fly";
+//   console.log(`user poser ${power}`);
+// }
+
+const balance = 1000;
+// if (balance > 500) console.log("tse");
+
+// if (balance < 500) {
+//     console.log("less than 500");
+// } else if (balance < 750) {
+//     console.log("less than 750");
+
+// } else if (balance < 900) {
+//     console.log("less than 750");
+
+// } else {
+//     console.log("less than 1200");
+
+// }
+
+const userLoggedIn = true;
+const debitCard = true;
+const loggedInFromGoogle = false;
+const loggedInFromEmail = true;
+
+if (userLoggedIn && debitCard && 2 == 3) {
+  console.log("Allow to buy course");
+}
+
+if (loggedInFromGoogle || loggedInFromEmail) {
+  console.log("User logged in");
+}
