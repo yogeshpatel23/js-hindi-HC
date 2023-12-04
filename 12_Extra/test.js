1019bccaeaaa8cd6c0f8d17b55ced4b4fe9fc7ff
Yogesh Patel <yogesh.d.p.patel@gmail.com>
2023-12-04 21:36:37 +0530

    Compleate on 04-12-23

---
+++ 12_Extra/test.js
@@ -0,0 +1,7 @@
+// const myArr = [];
+// % DebugPrint(myArr);
+
+/**
+ * for this watch video again
+ * https://www.youtube.com/watch?v=ZRS485LxX0s&list=PLu71SKxNbfoBuX3f4EOACle2y-tRC5Q37&index=52
+ */
