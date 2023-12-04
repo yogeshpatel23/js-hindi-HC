f2a65f350ad61ffdfb82f837a33a1cb412d3172c
Yogesh Patel <yogesh.d.p.patel@gmail.com>
2023-12-04 10:47:00 +0530

    more about objects

---
+++ 10_classes_and_oop/mathpi.js
@@ -0,0 +1,35 @@
+const discptroe = Object.getOwnPropertyDescriptor(Math, "PI");
+
+// console.log(discptroe);
+
+// console.log(Math.PI);
+// Math.PI = 5;
+// console.log(Math.PI);
+
+// const myNewObect = Object.create()
+
+const chai = {
+  name: "ginger chai",
+  price: 250,
+  isAvialble: true,
+};
+
+// console.log(Object.getOwnPropertyDescriptor(chai, "name"));
+
+// Object.defineProperty(chai, "name", {
+//   writable: false,
+//   enumerable: false,
+// });
+
+Object.defineProperties(chai, {
+  name: {
+    writable: false,
+    enumerable: false,
+  },
+  price: {
+    configurable: false,
+  },
+});
+
+console.log(Object.getOwnPropertyDescriptor(chai, "name"));
+// console.log(Object.getOwnPropertyDescriptors(Math));
